effb1040f099be695c84ab9bb0276fa20cd137bd
asadiqi <asadiqi@helb-prigogine.be>
2025-05-17 00:53:10 +0200

    Hotel constante enleveé

--- src/main/java/com/example/helbhotel/main/Hotel.java
+++ src/main/java/com/example/helbhotel/main/Hotel.java
@@ -1,21 +1,38 @@
 package com.example.helbhotel.main;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class Hotel {
 
-    private int amountOfFloors;
-    private List<Room[][]> building = new ArrayList<Room[][]>();
-    private Room[][] floor = new Room[4][4];
+    private  final int FLOOR_ROWS = 4;
+    private  final int FLOOR_COLUMNS = 4;
+    private static final String EMPTY_ROOM_TYPE = "Z";
+    private static final int ROOM_INFO_ARRAY_SIZE = 2;
+    private static final String LUXE_LABEL = "Luxe";
+    private static final String LUXE_COLOR = "#D8C4EC";
+    private static final String ECONOMIC_LABEL = "Economic";
+    private static final String ECONOMIC_COLOR = "#FFE5B4";
+    private static final String BUSINESS_LABEL = "Business";
+    private static final String BUSINESS_COLOR = "#BFDFFF";
+    private static final int LABEL_INDEX = 0;
+    private static final int COLOR_INDEX = 1;
+    private static final int INITIAL_ROOM_NUMBER = 1;
+    private static final int INITIAL_FLOOR_NUMBER = 0;
+    private static final int ALPHABET_LENGTH = 26;
+    private static final char FIRST_LETTER = 'A';
+    private static final int ALPHABET_START_OFFSET = 1;
+    private static final int FIRST_CHAR_INDEX = 0;
+    private static final int INITIAL_INDEX = 0;
+    private final int amountOfFloors;
+    private final List<Room[][]> building = new ArrayList<Room[][]>();
+    private Room[][] floor = new Room[FLOOR_ROWS][FLOOR_COLUMNS];
     private HConfigParser configParser;
     private ReservationParser reservationParser;
     private static final String HCONFIG_FILE_PATH = "C:\\Users\\sadiq\\Desktop\\javafinalq4\\src\\main\\java\\com\\example\\helbhotel\\main\\hconfig";
     private static final String CSV_FILE_PATH = "C:\\Users\\sadiq\\Desktop\\javafinalq4\\src\\main\\java\\com\\example\\helbhotel\\main\\reservation.csv";
-
     private List<Reservation> reservations = new ArrayList<>();
     private List<RoomAssignmentSuggestion> roomAssignmentSuggestions = new ArrayList<>();
+
 
     public Hotel() {
         configParser = new HConfigParser(HCONFIG_FILE_PATH);
@@ -23,17 +40,17 @@ public class Hotel {
         amountOfFloors = configParser.getNombreEtages();
         List<List<String>> floorData = configParser.getChambreConfig();
 
-        for (int floorNumber = 0; floorNumber < amountOfFloors; floorNumber++) {
-            this.floor = new Room[4][4];
-            int roomNumber = 1;
-            for (int i = 0; i < floorData.size(); i++) {
-                for (int j = 0; j < floorData.get(i).size(); j++) {
+        for (int floorNumber = INITIAL_FLOOR_NUMBER; floorNumber < amountOfFloors; floorNumber++) {
+            this.floor = new Room[FLOOR_ROWS][FLOOR_COLUMNS];
+            int roomNumber = INITIAL_ROOM_NUMBER ;
+            for (int i = INITIAL_INDEX; i < floorData.size(); i++) {
+                for (int j = INITIAL_INDEX; j < floorData.get(i).size(); j++) {
                     String roomChar = floorData.get(i).get(j);
-                    this.floor[i][j] = new RoomFactory().createRoom(roomChar.charAt(0),
+                    this.floor[i][j] = new RoomFactory().createRoom(roomChar.charAt(FIRST_CHAR_INDEX ),
                             getFloorLabel(floorNumber),
                             roomNumber);
 
-                    if (roomChar.equals("Z"))
+                    if (roomChar.equals(EMPTY_ROOM_TYPE))
                         continue;
                     roomNumber++;
 
@@ -52,12 +69,12 @@ public class Hotel {
         reservationParser.deleteLineInFile(CSV_FILE_PATH,
                 roomAssignmentSuggestion.getReservation().setToCsvLineFormat());
 
-        for (int floorNumber = 0; floorNumber < this.building.size(); floorNumber++) {
+        for (int floorNumber = INITIAL_FLOOR_NUMBER; floorNumber < this.building.size(); floorNumber++) {
             Room[][] floorData = this.building.get(floorNumber);
-            for (int i = 0; i < floorData.length; i++) {
-                for (int j = 0; j < floorData[i].length; j++) {
+            for (int i = INITIAL_INDEX ; i < floorData.length; i++) {
+                for (int j = INITIAL_INDEX ; j < floorData[i].length; j++) {
                     Room room = floorData[i][j];
-                    if (room.getRoomType().equals("Z"))
+                    if (room.getRoomType().equals(EMPTY_ROOM_TYPE))
                         continue;
 
                     if (floorData[i][j].getName().equals(roomAssignmentSuggestion.getRoom().getName())) {
@@ -74,12 +91,12 @@ public class Hotel {
     }
 
     public void freeRoom(Room roomToFree) {
-        for (int floorNumber = 0; floorNumber < this.building.size(); floorNumber++) {
+        for (int floorNumber = INITIAL_FLOOR_NUMBER; floorNumber < this.building.size(); floorNumber++) {
             Room[][] floorData = this.building.get(floorNumber);
-            for (int i = 0; i < floorData.length; i++) {
-                for (int j = 0; j < floorData[i].length; j++) {
+            for (int i = INITIAL_INDEX ; i < floorData.length; i++) {
+                for (int j = INITIAL_INDEX ; j < floorData[i].length; j++) {
                     Room room = floorData[i][j];
-                    if (room.getRoomType().equals("Z"))
+                    if (room.getRoomType().equals(EMPTY_ROOM_TYPE))
                         continue;
 
                     if (floorData[i][j].getName().equals(roomToFree.getName())) {
@@ -96,17 +113,17 @@ public class Hotel {
         List<String[]> roomInformation = new ArrayList<>();
 
         // Create some string arrays with 2 places each
-        String[] luxeRoom = new String[2];
-        luxeRoom[0] = "Luxe";
-        luxeRoom[1] = "#D8C4EC";
+        String[] luxeRoom = new String[ROOM_INFO_ARRAY_SIZE];
+        luxeRoom[LABEL_INDEX] = LUXE_LABEL;
+        luxeRoom[COLOR_INDEX] = LUXE_COLOR;
 
-        String[] economicRoom = new String[2];
-        economicRoom[0] = "Economic";
-        economicRoom[1] = "#FFE5B4";
+        String[] economicRoom = new String[ROOM_INFO_ARRAY_SIZE];
+        economicRoom[LABEL_INDEX] = ECONOMIC_LABEL;
+        economicRoom[COLOR_INDEX] = ECONOMIC_COLOR;
 
-        String[] businessRoom = new String[2];
-        businessRoom[0] = "Business";
-        businessRoom[1] = "#BFDFFF";
+        String[] businessRoom = new String[ROOM_INFO_ARRAY_SIZE];
+        businessRoom[LABEL_INDEX] = BUSINESS_LABEL;
+        businessRoom[COLOR_INDEX] = BUSINESS_COLOR;
 
         // Add the arrays to the list
         roomInformation.add(luxeRoom);
@@ -119,12 +136,12 @@ public class Hotel {
     public List<Room> getRooms() {
         List<Room> roomList = new ArrayList<Room>();
 
-        for (int floorNumber = 0; floorNumber < this.building.size(); floorNumber++) {
+        for (int floorNumber = INITIAL_FLOOR_NUMBER; floorNumber < this.building.size(); floorNumber++) {
             Room[][] floorData = this.building.get(floorNumber);
-            for (int i = 0; i < floorData.length; i++) {
-                for (int j = 0; j < floorData[i].length; j++) {
+            for (int i = INITIAL_INDEX; i < floorData.length; i++) {
+                for (int j = INITIAL_INDEX; j < floorData[i].length; j++) {
                     Room room = floorData[i][j];
-                    if (room.getRoomType().equals("Z"))
+                    if (room.getRoomType().equals(EMPTY_ROOM_TYPE))
                         continue;
                     roomList.add(room);
                 }
@@ -137,10 +154,10 @@ public class Hotel {
     public static String getFloorLabel(int n) {
         StringBuilder sb = new StringBuilder();
         n++;
-        while (n > 0) {
+        while (n > INITIAL_INDEX) {
             n--;
-            sb.insert(0, (char) ('A' + (n % 26)));
-            n /= 26;
+            sb.insert(INITIAL_INDEX, (char) (FIRST_LETTER  + (n % ALPHABET_LENGTH)));
+            n /= ALPHABET_LENGTH;
         }
         return sb.toString();
     }
@@ -152,64 +169,16 @@ public class Hotel {
 
 
     public static int getFloorIndex(String label) {
-        int result = 0;
-        for (int i = 0; i < label.length(); i++) {
-            result *= 26;
-            result += label.charAt(i) - 'A' + 1;
+        int result = INITIAL_INDEX;
+        for (int i = INITIAL_INDEX; i < label.length(); i++) {
+            result *= ALPHABET_LENGTH;
+            result += label.charAt(i) - FIRST_LETTER  + ALPHABET_START_OFFSET;
         }
-        return result - 1;
+        return result - ALPHABET_START_OFFSET;
     }
 
     public int getAmountOfFloors() {
         return configParser.getNombreEtages();
-    }
-
-    public boolean areAllRoomsBooked() {
-        for (int i = 0; i < building.size(); i++) {
-            Room[][] floor = building.get(i);
-            for (int j = 0; j < floor.length; j++) {
-                for (int k = 0; k < floor[j].length; k++) {
-                    if (floor[i][j].getRoomType().equals("Z")) {
-                        continue;
-                    }
-
-                    if (floor[i][j].isAvailable()) {
-                        return false;
-                    }
-                }
-            }
-        }
-
-        return true;
-    }
-
-    public List<Room> giveRandomAvailableRooms(int count) {
-        List<Room> availableRooms = new ArrayList<>();
-
-        for (Room[][] floor : building) {
-            for (int i = 0; i < floor.length; i++) {
-                for (int j = 0; j < floor[i].length; j++) {
-                    if (floor[i][j] != null && floor[i][j].isAvailable()) {
-                        availableRooms.add(floor[i][j]);
-                    }
-                }
-            }
-        }
-
-        if (availableRooms.size() <= count) {
-            return availableRooms;
-        }
-
-        List<Room> result = new ArrayList<>();
-        Random random = new Random();
-
-        while (result.size() < count && !availableRooms.isEmpty()) {
-            int randomIndex = random.nextInt(availableRooms.size());
-            result.add(availableRooms.remove(randomIndex));
-        }
-
-        return result;
-
     }
 
     public List<Room[][]> getBuilding() {
