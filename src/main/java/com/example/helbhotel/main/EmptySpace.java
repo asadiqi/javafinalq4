262f9b065d0ea171cf17b63ea9674a4b4ac6f331
asadiqi <asadiqi@helb-prigogine.be>
2025-05-16 22:00:13 +0200

    encore a faire

--- src/main/java/com/example/helbhotel/main/EmptySpace.java
+++ src/main/java/com/example/helbhotel/main/EmptySpace.java
@@ -3,8 +3,11 @@ package com.example.helbhotel.main;
 
 public class EmptySpace extends Room {
 
+    private static final int INVALID_ROOM_NUMBER = -1;
+    private static final char TYPE_CODE = 'Z';
+
     public EmptySpace(String floor) {
-        super(floor, -1);
-        this.roomType = 'Z';
+        super(floor,INVALID_ROOM_NUMBER );
+        this.roomType = TYPE_CODE;
     }
 }
